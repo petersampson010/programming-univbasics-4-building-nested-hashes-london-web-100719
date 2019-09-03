a135ce5260c3ed1bde7e89025bc0482556781d38
Peter Sampson <petersampson010@gmail.com>
2019-09-03 15:54:00 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -22,7 +22,7 @@ def monopoly_with_third_tier
       },
       :names => {
         :reading_railroad => {},
-        :key2 => {},
+        :pennsylvania => {},
         :key3 => {},
         :key4 => {}
       }
