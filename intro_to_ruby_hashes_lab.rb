07b3c33b4f30c08496ecb8871fb4002d3a09274d
Peter Sampson <petersampson010@gmail.com>
2019-09-03 15:45:09 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -14,7 +14,9 @@ def monopoly_with_third_tier
   baseHash = {
     :railroads => {
       :pieces => 4,
-      :rent_in_dollars => {}
+      :rent_in_dollars => {
+        :key1
+      }
     }
   }
 end
